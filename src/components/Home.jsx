f179426aa149e22bfc7141267160eedbff7bb97f
Eben <freddy.eben@gmail.com>
2024-06-20 14:18:44 +0700

    fix padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -751,10 +751,7 @@ function Home() {
 									</a>
 								</div>
 							</div> */}
-              {/* <div className="mt-8 home-intro-text">
-                Legacy $CREO staking and farming pools are closed. The new pools
-                are open with Seed Staking Boosters. You can deposit now.
-              </div> */}
+              <div className="mt-8 px-80"></div>
             </div>
             <div className="home-staking-box">
               <div className="head-box">
